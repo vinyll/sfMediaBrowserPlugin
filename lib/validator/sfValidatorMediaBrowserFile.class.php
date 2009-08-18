3f33fb25aa3dd55f8470e72a8418ae2da7c65f90
Vincent <contact@onanga.com>
2009-08-18 11:25:13 +0200

    add form validator

---
+++ lib/validator/sfValidatorMediaBrowserFile.class.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * (c) Vincent Agnano <vince@onanga.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfValidatorMediaBrowser validates a selected file.
+ *
+ * @package    sfMediaBrowserPlugin
+ * @subpackage validator
+ * @author     Fabien Potencier <vince@onanga.com>
+ */
+class sfValidatorMediaBrowserFile extends sfValidatorBase
+{
+  /**
+   * Configures the current validator.
+   *
+   * Available options:
+   *
+   *  * root_dir: The upload root directory
+   *
+   * Available error codes:
+   *
+   *  * root_dir
+   *
+   * @param array $options   An array of options
+   * @param array $messages  An array of error messages
+   *
+   * @see sfValidatorBase
+   */
+  protected function configure($options = array(), $messages = array())
+  {
+    $this->setMessage('invalid', '"%file%" is not a valid file path.');    
+    $this->addOption('type');
+    $this->addMessage('type', 'the file must be a "%type%"');
+  }
+
+  /**
+   * @see sfValidatorBase
+   */
+  protected function doClean($value)
+  {
+    $full_path = sfConfig::get('sf_web_dir').'/'.$value;
+    if(!is_file($full_path))
+    {
+      throw new sfValidatorError($this, 'invalid', array('file' => $value));
+    }
+    
+    return $value;
+  }
+}
