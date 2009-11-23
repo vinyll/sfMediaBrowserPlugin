9d5b22a6021cd436dc3861840f67566b16b6d3f5
Vincent Agnano <contact@onanga.com>
2009-11-23 19:02:03 +0100

    fix sf1.3/1.4 stylesheets and js not being included

--- modules/sfMediaBrowser/templates/popupLayout.php
+++ modules/sfMediaBrowser/templates/popupLayout.php
@@ -6,6 +6,9 @@
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <?php include_stylesheets() ?>
+    <?php include_javascripts() ?>
   </head>
   <body>
     <?php echo $sf_content ?>
