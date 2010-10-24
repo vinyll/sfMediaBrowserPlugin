dbb43c19cf6e29819a1ff330c44d549944f649c5
Maastermedia <peter.kokot@m-m.si>
2010-10-23 17:20:28 -0700

    Variable name typo fixed: $curent_path should be named $current_path.

--- modules/sfMediaBrowser/lib/BasesfMediaBrowserActions.class.php
+++ modules/sfMediaBrowser/lib/BasesfMediaBrowserActions.class.php
@@ -144,7 +144,7 @@ class BasesfMediaBrowserActions extends sfActions
     $moved = @rename($current_path, $new_path);
     if(!$moved)
     {
-      $this->logMessage(sprintf('Failed renaming "%s" to "%s".', $curent_path, $new_path), 'err');
+      $this->logMessage(sprintf('Failed renaming "%s" to "%s".', $current_path, $new_path), 'err');
     }
     
     if($request->isXmlHttpRequest())
