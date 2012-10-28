5c8fb92f0bc9dcfea5b74c3299a3af2685d37949
Ain Tohvri <at@interactive-pioneers.de>
2012-10-28 18:06:08 +0100

    Fixes file select handler issue #18 on vinyll's original repository

--- web/js/WindowManager.js
+++ web/js/WindowManager.js
@@ -7,59 +7,32 @@
  */
 sfMediaBrowserWindowManager = {
 
-  browser_url:  null,
+    browser_url:  null,
 
-  init: function(browser_url) {
-    if(browser_url == null || browser_url == '')
-      throw new Error('sfMediaBrowserWindowManager.init() requires one parameter that is the url for browser popup window');
-    sfMediaBrowserWindowManager.browser_url = browser_url;
-  },
+    init: function(browser_url) {
+        if(browser_url == null || browser_url == '')
+            throw new Error('sfMediaBrowserWindowManager.init() requires one parameter that is the url for browser popup window');
+        sfMediaBrowserWindowManager.browser_url = browser_url;
+    },
   
-  open: function(params) {
-    var width = window.innerWidth ? window.innerWidth*.8 : 500;
-    params['popup'] = window.open(params['url'], 'sfMediaBrowser', 'width='+width+',addressbar=0,scrollbars=1');
-    return new sfMediaBrowserWindowManagerObject(params);
-  },
-  
-  addListerner: function(params) {
-    var event = params['event'] ? params['event'] : 'onclick';
-    params['target'] = document.getElementById(params['target']);
-    params['target'][event] = function() {
-      var window_manager = sfMediaBrowserWindowManager.open(params);
-      window.window_manager = window_manager;
+    tinymceCallback: function(field_name, url, type, win) 
+    {
+        if (sfMediaBrowserWindowManager.browser_url == '')
+            throw new Error('You must initialise sfMediaBrowserWindowManager.init($browser_url) when calling browser for tinymce');
+        
+        tinyMCE.activeEditor.windowManager.open(
+        {
+            file: sfMediaBrowserWindowManager.browser_url,
+            title: 'File Browser',
+            width: 420,
+            height: 400,
+            resizable: "yes",
+            inline: "yes",
+            close_previous: "no"
+        }, 
+        {
+            window: win,
+            input: field_name
+        });
     }
-  },
-  
-  tinymceCallback: function(field_name, url, type, win) {
-    if(sfMediaBrowserWindowManager.browser_url == '')
-      throw new Error('You must initialise sfMediaBrowserWindowManager.init($browser_url) when calling browser for tinymce');
-    var window_manager = sfMediaBrowserWindowManager.open({
-      target: win.document.getElementById(field_name),
-      url:    sfMediaBrowserWindowManager.browser_url
-    });
-    win.onunload = function() {
-      window_manager.popup.close();
-    }
-    window_manager.popup.opener = win;
-    window_manager.popup.opener.window_manager = window_manager;
-  }
-  
 };
-
-function sfMediaBrowserWindowManagerObject(params) {
-  this.target = params['target'];
-  this.popup = params['popup'];
-}
-
-sfMediaBrowserWindowManagerObject.prototype.callback = function(value) {
-  this.getTarget().value = value;
-  this.getTarget().onchange();
-  this.popup.close();
-}
-sfMediaBrowserWindowManagerObject.prototype.getTarget = function() {
-  if(this.target.getAttribute)
-    return this.target;
-  else
-    return this.popup.opener.document.getElementById(this.target);
-}
-
